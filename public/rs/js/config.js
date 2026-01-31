d158dae5691c5f8ff0f0c708e6e899b5de4b7d07
jpatt0n <jpatton624@gmail.com>
2026-01-31 17:25:35 -0600

    Add default ICE servers configuration: set fallback STUN server for RTC connections when no custom servers are provided in global config.

--- public/rs/js/config.js
+++ public/rs/js/config.js
@@ -1,5 +1,6 @@
 const globalConfig = window.RENDER_STREAMING_CONFIG || {};
 const signalingBaseUrl = (globalConfig.signalingBaseUrl || location.origin).replace(/\/$/, '');
+const DEFAULT_ICE_SERVERS = [{ urls: ['stun:stun.l.google.com:19302'] }];
 
 export async function getServerConfig() {
   const protocolEndPoint = signalingBaseUrl + '/config';
@@ -10,6 +11,10 @@ export async function getServerConfig() {
 export function getRTCConfiguration() {
   let config = {};
   config.sdpSemantics = 'unified-plan';
-  config.iceServers = globalConfig.iceServers || [];
+  if (Array.isArray(globalConfig.iceServers) && globalConfig.iceServers.length > 0) {
+    config.iceServers = globalConfig.iceServers;
+  } else {
+    config.iceServers = DEFAULT_ICE_SERVERS;
+  }
   return config;
 }
