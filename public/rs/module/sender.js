c35c5216695d85f6da1a4ff96665b90538b42368
jpatt0n <jpatton624@gmail.com>
2026-02-15 21:27:22 -0600

    Refactor video player and sender input handling: remove unused keyboard and visibility change event listeners, streamline input management, and enhance event binding for mouse and keyboard actions. Improve cleanup processes during player deletion and optimize keyboard event handling in the Sender class.

--- public/rs/module/sender.js
+++ public/rs/module/sender.js
@@ -18,28 +18,16 @@ export class Sender extends LocalInputManager {
     this._elem = elem;
     this._loggedMouseEvent = false;
     this._loggedKeyEvent = false;
-    this._pressedKeys = new Set();
-    this._altAsControlFallback = false;
     this._corrector = new PointerCorrector(
       this._elem.videoWidth,
       this._elem.videoHeight,
       this._elem
       );
 
-    this._onResizeEventHandler = this._onResizeEvent.bind(this);
-    this._onMouseEventHandler = this._onMouseEvent.bind(this);
-    this._onWheelEventHandler = this._onWheelEvent.bind(this);
-    this._onKeyEventHandler = this._onKeyEvent.bind(this);
-    this._onGamepadEventHandler = this._onGamepadEvent.bind(this);
-    this._onTouchEventHandler = this._onTouchEvent.bind(this);
-    this._onWindowBlurHandler = this._onWindowBlur.bind(this);
-    this._onPageHideHandler = this._onPageHide.bind(this);
-    this._onVisibilityChangeHandler = this._onVisibilityChange.bind(this);
-
     //since line 27 cannot complete resize initialization but can only monitor div dimension changes, line 26 needs to be reserved
-    this._elem.addEventListener('resize', this._onResizeEventHandler, false);
-    this._resizeObserver = new ResizeObserver(this._onResizeEventHandler);
-    this._resizeObserver.observe(this._elem);
+    this._elem.addEventListener('resize', this._onResizeEvent.bind(this), false);
+    const observer = new ResizeObserver(this._onResizeEvent.bind(this));
+    observer.observe(this._elem);
   }
 
   addMouse() {
@@ -55,11 +43,11 @@ export class Sender extends LocalInputManager {
     this.mouse = new Mouse("Mouse", "Mouse", 1, null, descriptionMouse);
     this._devices.push(this.mouse);
 
-    this._elem.addEventListener('click', this._onMouseEventHandler, false);
-    this._elem.addEventListener('mousedown', this._onMouseEventHandler, false);
-    this._elem.addEventListener('mouseup', this._onMouseEventHandler, false);
-    this._elem.addEventListener('mousemove', this._onMouseEventHandler, false);
-    this._elem.addEventListener('wheel', this._onWheelEventHandler, false);
+    this._elem.addEventListener('click', this._onMouseEvent.bind(this), false);
+    this._elem.addEventListener('mousedown', this._onMouseEvent.bind(this), false);
+    this._elem.addEventListener('mouseup', this._onMouseEvent.bind(this), false);
+    this._elem.addEventListener('mousemove', this._onMouseEvent.bind(this), false);
+    this._elem.addEventListener('wheel', this._onWheelEvent.bind(this), false);
   }
 
   addKeyboard() {
@@ -75,20 +63,8 @@ export class Sender extends LocalInputManager {
     this.keyboard = new Keyboard("Keyboard", "Keyboard", 2, null, descriptionKeyboard);
     this._devices.push(this.keyboard);
 
-    document.addEventListener('keyup', this._onKeyEventHandler, false);
-    document.addEventListener('keydown', this._onKeyEventHandler, false);
-    window.addEventListener('blur', this._onWindowBlurHandler, false);
-    window.addEventListener('pagehide', this._onPageHideHandler, false);
-    document.addEventListener('visibilitychange', this._onVisibilityChangeHandler, false);
-  }
-
-  setAltAsControlFallback(enabled) {
-    const next = !!enabled;
-    if (this._altAsControlFallback === next) {
-      return;
-    }
-    this.releaseAllInputs();
-    this._altAsControlFallback = next;
+    document.addEventListener('keyup', this._onKeyEvent.bind(this), false);
+    document.addEventListener('keydown', this._onKeyEvent.bind(this), false);
   }
 
   addGamepad() {
@@ -104,10 +80,10 @@ export class Sender extends LocalInputManager {
     this.gamepad = new Gamepad("Gamepad", "Gamepad", 3, null, descriptionGamepad);
     this._devices.push(this.gamepad);
 
-    window.addEventListener("gamepadconnected", this._onGamepadEventHandler, false);
-    window.addEventListener("gamepaddisconnected", this._onGamepadEventHandler, false);
+    window.addEventListener("gamepadconnected", this._onGamepadEvent.bind(this), false);
+    window.addEventListener("gamepaddisconnected", this._onGamepadEvent.bind(this), false);
     this._gamepadHandler = new GamepadHandler();
-    this._gamepadHandler.addEventListener("gamepadupdated", this._onGamepadEventHandler, false);
+    this._gamepadHandler.addEventListener("gamepadupdated", this._onGamepadEvent.bind(this), false);
   }
 
   addTouchscreen() {
@@ -123,11 +99,11 @@ export class Sender extends LocalInputManager {
     this.touchscreen = new Touchscreen("Touchscreen", "Touchscreen", 4, null, descriptionTouch);
     this._devices.push(this.touchscreen);
 
-    this._elem.addEventListener('touchend', this._onTouchEventHandler, false);
-    this._elem.addEventListener('touchstart', this._onTouchEventHandler, false);
-    this._elem.addEventListener('touchcancel', this._onTouchEventHandler, false);
-    this._elem.addEventListener('touchmove', this._onTouchEventHandler, false);
-    this._elem.addEventListener('click', this._onTouchEventHandler, false);
+    this._elem.addEventListener('touchend', this._onTouchEvent.bind(this), false);
+    this._elem.addEventListener('touchstart', this._onTouchEvent.bind(this), false);
+    this._elem.addEventListener('touchcancel', this._onTouchEvent.bind(this), false);
+    this._elem.addEventListener('touchmove', this._onTouchEvent.bind(this), false);
+    this._elem.addEventListener('click', this._onTouchEvent.bind(this), false);
   }
 
   /**
@@ -165,14 +141,9 @@ export class Sender extends LocalInputManager {
     this._queueStateEvent(this.mouse.currentState, this.mouse);
   }
   _onKeyEvent(event) {
-    const code = this._resolveKeyCode(event);
-    if (!code) {
-      return;
-    }
     if(event.type == 'keydown') {
       if(!event.repeat) { // StateEvent
-        this._pressedKeys.add(code);
-        this.keyboard.queueEvent({ type: 'keydown', code: code });
+        this.keyboard.queueEvent(event);
         this._queueStateEvent(this.keyboard.currentState, this.keyboard);
         if (!this._loggedKeyEvent) {
           this._loggedKeyEvent = true;
@@ -184,26 +155,9 @@ export class Sender extends LocalInputManager {
       }
     }
     else if(event.type == 'keyup') {
-      this._pressedKeys.delete(code);
-      this.keyboard.queueEvent({ type: 'keyup', code: code });
+      this.keyboard.queueEvent(event);
       this._queueStateEvent(this.keyboard.currentState, this.keyboard);
     }
-  }
-
-  _resolveKeyCode(event) {
-    if (!event || !event.code) {
-      return null;
-    }
-    if (!this._altAsControlFallback) {
-      return event.code;
-    }
-    if (event.code === 'AltLeft') {
-      return 'ControlLeft';
-    }
-    if (event.code === 'AltRight') {
-      return 'ControlRight';
-    }
-    return event.code;
   }
   _isTextInputKey(event) {
     if (!event || !event.key) {
@@ -243,92 +197,6 @@ export class Sender extends LocalInputManager {
         this._queueStateEvent(this.gamepad.currentState, this.gamepad);
         break;
       }
-    }
-  }
-
-  _onWindowBlur() {
-    this.releaseAllInputs();
-  }
-
-  _onPageHide() {
-    this.releaseAllInputs();
-  }
-
-  _onVisibilityChange() {
-    if (document.visibilityState === 'hidden') {
-      this.releaseAllInputs();
-    }
-  }
-
-  _releaseAllKeys() {
-    if (!this.keyboard || this._pressedKeys.size === 0) {
-      return;
-    }
-    const keysToRelease = Array.from(this._pressedKeys);
-    this._pressedKeys.clear();
-    for (const code of keysToRelease) {
-      this.keyboard.queueEvent({ type: 'keyup', code: code });
-      this._queueStateEvent(this.keyboard.currentState, this.keyboard);
-    }
-  }
-
-  _releaseMouseButtons() {
-    if (!this.mouse) {
-      return;
-    }
-    const hasButtonsDown =
-      this.mouse.currentState != null
-      && this.mouse.currentState.buttons != null
-      && new Uint16Array(this.mouse.currentState.buttons)[0] !== 0;
-    if (!hasButtonsDown) {
-      return;
-    }
-    const state = this.mouse.currentState;
-    const event = {
-      type: 'mouseup',
-      button: 0,
-      buttons: 0,
-      clientX: state.position[0],
-      clientY: state.position[1],
-      movementX: 0,
-      movementY: 0,
-    };
-    this.mouse.queueEvent(event);
-    this._queueStateEvent(this.mouse.currentState, this.mouse);
-  }
-
-  releaseAllInputs() {
-    this._releaseAllKeys();
-    this._releaseMouseButtons();
-  }
-
-  dispose() {
-    this.releaseAllInputs();
-    this._elem.removeEventListener('resize', this._onResizeEventHandler, false);
-    if (this._resizeObserver) {
-      this._resizeObserver.disconnect();
-      this._resizeObserver = null;
-    }
-    this._elem.removeEventListener('click', this._onMouseEventHandler, false);
-    this._elem.removeEventListener('mousedown', this._onMouseEventHandler, false);
-    this._elem.removeEventListener('mouseup', this._onMouseEventHandler, false);
-    this._elem.removeEventListener('mousemove', this._onMouseEventHandler, false);
-    this._elem.removeEventListener('wheel', this._onWheelEventHandler, false);
-    this._elem.removeEventListener('touchend', this._onTouchEventHandler, false);
-    this._elem.removeEventListener('touchstart', this._onTouchEventHandler, false);
-    this._elem.removeEventListener('touchcancel', this._onTouchEventHandler, false);
-    this._elem.removeEventListener('touchmove', this._onTouchEventHandler, false);
-    this._elem.removeEventListener('click', this._onTouchEventHandler, false);
-    document.removeEventListener('keyup', this._onKeyEventHandler, false);
-    document.removeEventListener('keydown', this._onKeyEventHandler, false);
-    document.removeEventListener('visibilitychange', this._onVisibilityChangeHandler, false);
-    window.removeEventListener('blur', this._onWindowBlurHandler, false);
-    window.removeEventListener('pagehide', this._onPageHideHandler, false);
-    window.removeEventListener("gamepadconnected", this._onGamepadEventHandler, false);
-    window.removeEventListener("gamepaddisconnected", this._onGamepadEventHandler, false);
-    if (this._gamepadHandler) {
-      this._gamepadHandler.removeEventListener("gamepadupdated", this._onGamepadEventHandler, false);
-      this._gamepadHandler = null;
     }
   }
 
